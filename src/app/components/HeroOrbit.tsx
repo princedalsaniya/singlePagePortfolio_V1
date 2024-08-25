18936d2d4a7c14275072d5e9b0eef49de1ea1703
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-25 18:01:27 +0530

    feat: Added more sparkles and circles.
    - Added a fade gradient on top and bottom for section change.

--- src/app/components/HeroOrbit.tsx
+++ src/app/components/HeroOrbit.tsx
@@ -8,7 +8,10 @@ interface HeroOrbitProps {
 export const HeroOrbit = ({ children, size, rotation }: PropsWithChildren<HeroOrbitProps>) => {
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
-      <div style={{ height: `${size}px`, width: `${size}px`, transform: `rotate(${rotation}deg)` }}>
+      <div
+        className="flex items-start justify-start"
+        style={{ height: `${size}px`, width: `${size}px`, transform: `rotate(${rotation}deg)` }}
+      >
         <div className="inline-flex" style={{ transform: `rotate(${rotation * -1}deg)` }}>
           {children}
         </div>
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -2,34 +2,63 @@ import PrinceAnimation from '@/app/assets/images/PrinceAnimation.gif';
 import ArrowDown from '@/app/assets/icons/arrow-down.svg';
 import grainImage from '@/app/assets/images/grain.jpg';
 import StarIcon from '@/app/assets/icons/star.svg';
+import SparkleIcon from '@/app/assets/icons/sparkle.svg';
 import Image from 'next/image';
 import { HeroOrbit } from '../components/HeroOrbit';
 
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z- overflow-x-clip">
-      {/* Grain texture Image */}
-      <div
-        className="absolute inset-0 -z-30 opacity-5"
-        style={{
-          backgroundImage: `url(${grainImage.src})`,
-        }}
-      ></div>
+      {/* Whole Background */}
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
+        {/* Grain texture Effect */}
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
 
-      {/* Emerald colored rings */}
-      <div className="size-[620px] hero-ring" />
-      <div className="size-[820px] hero-ring" />
-      <div className="size-[1020px] hero-ring" />
-      <div className="size-[1220px] hero-ring" />
-      <HeroOrbit size={800} rotation={-72}>
-        <StarIcon className="size-28 text-emerald-300" />
-      </HeroOrbit>
-      <HeroOrbit size={550} rotation={20}>
-        <StarIcon className="size-12 text-emerald-300" />
-      </HeroOrbit>
-      <HeroOrbit size={590} rotation={98}>
-        <StarIcon className="size-8 text-emerald-300" />
-      </HeroOrbit>
+        {/* Emerald colored rings */}
+        <div className="size-[620px] hero-ring" />
+        <div className="size-[820px] hero-ring" />
+        <div className="size-[1020px] hero-ring" />
+        <div className="size-[1220px] hero-ring" />
+
+        {/* Decorations : Stars, Sparkles, SmallCircles */}
+        <div>
+          <HeroOrbit size={800} rotation={-72}>
+            <StarIcon className="size-28 text-emerald-300" />
+          </HeroOrbit>
+          <HeroOrbit size={550} rotation={20}>
+            <StarIcon className="size-12 text-emerald-300" />
+          </HeroOrbit>
+          <HeroOrbit size={590} rotation={98}>
+            <StarIcon className="size-8 text-emerald-300" />
+          </HeroOrbit>
+          <HeroOrbit size={430} rotation={-14}>
+            <SparkleIcon className="size-8 text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={440} rotation={79}>
+            <SparkleIcon className="size-5 text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={530} rotation={178}>
+            <SparkleIcon className="size-10 text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={710} rotation={144}>
+            <SparkleIcon className="size-14 text-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={720} rotation={85}>
+            <div className="size-3 rounded-full bg-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={520} rotation={-41}>
+            <div className="size-2 rounded-full bg-emerald-300/20" />
+          </HeroOrbit>
+          <HeroOrbit size={650} rotation={-5}>
+            <div className="size-2 rounded-full bg-emerald-300/20" />
+          </HeroOrbit>
+        </div>
+      </div>
 
       {/* Actual content */}
       <div className="container">
