f4a0c97efbc5ee94ac5af8b635d84e82e0a2400c
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-25 16:45:55 +0530

    build: Hero Section
    - Added HTML code for the section
    - Added an GIF for home page

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,3 +1,32 @@
+import PrinceAnimation from '@/app/assets/images/PrinceAnimation.gif';
+import ArrowDown from '@/app/assets/icons/arrow-down.svg';
+import Image from 'next/image';
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div>
+      <div className="container">
+        <Image src={PrinceAnimation} alt="A person enjoying his music in earphones" />
+        <div>
+          <div></div>
+          <div>Available for new projects</div>
+        </div>
+        <h1>Translate Designs to Real Websites</h1>
+        <p>
+          I specialize in translating the designs into functional, high-performing webpages. Let's discuss your
+          next project.
+        </p>
+        <div>
+          <button>
+            <span>Explore My Work</span>
+            <ArrowDown />
+          </button>
+          <button>
+            <span>👋🏻</span>
+            <span>Let's Connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
