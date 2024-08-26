ee5c76050de443438b4b1b96e47dc03e8b4ecc2a
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-26 10:00:13 +0530

    style: for Tablet view

--- src/app/sections/Projects.tsx
+++ src/app/sections/Projects.tsx
@@ -55,14 +55,14 @@ export const ProjectsSection = () => {
             Real-world Projects
           </p>
         </div>
-        <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
-        <p className="text-center text-white/60 mt-4">
+        <h2 className="font-serif text-3xl text-center mt-6 md:text-5xl">Featured Projects</h2>
+        <p className="text-center text-white/60 mt-4 md:text-lg max-w-md mx-auto">
           See how I transformed Figma designs into engaging digital experiences.
         </p>
-        <div className="flex flex-col mt-10 gap-20">
+        <div className="flex flex-col mt-10 gap-20 md:mt-20">
           {portfolioProjects.map((project) => (
             <div
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:content-[''] after:absolute after:inset-0 after:z-10 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none"
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:content-[''] after:absolute after:inset-0 after:z-10 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none"
               key={project.title}
             >
               <div
@@ -75,25 +75,25 @@ export const ProjectsSection = () => {
                 <span>{project.year}</span>
               </div>
 
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4" />
-              <ul className="flex flex-col gap-4 mt-4">
+              <h3 className="font-serif text-2xl md:text-4xl mt-2 md:mt-5">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                 {project.results.map((result) => (
                   <span>
-                    <li className="flex gap-2 text-sm text-white/50">
-                      <CheckCircleIcon className="size-5 shrink-0" />
+                    <li className="flex gap-2 text-sm md:text-base text-white/50 items-center">
+                      <CheckCircleIcon className="size-5 md:size-6 shrink-0" />
                       {result.title}
                     </li>
                   </span>
                 ))}
               </ul>
               <a href={project.link}>
-                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold  inline-flex justify-center items-center gap-2 mt-8">
+                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold  inline-flex justify-center items-center gap-2 mt-8">
                   <span>View Live Site</span>
                   <ArrowUpRightIcon className="size-4" />
                 </button>
               </a>
-              <Image className="mt-8 -mb-4" src={project.image} alt={project.title}></Image>
+              <Image className="mt-8 -mb-4 md:-mb-0" src={project.image} alt={project.title}></Image>
             </div>
           ))}
         </div>
