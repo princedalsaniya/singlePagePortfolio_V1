50e92c8fc39dd8b5d06717d2dff670f215070522
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-26 10:34:18 +0530

    feat: added HTML content for Tape Section

--- src/app/sections/Tape.tsx
+++ src/app/sections/Tape.tsx
@@ -1,3 +1,28 @@
+import StarIcon from '@/app/assets/icons/star.svg';
+
+const words = [
+  'Performance',
+  'Accessible',
+  'Secure',
+  'Interactive',
+  'Scalable',
+  'User Friendly',
+  'Maintainable',
+  'Usable',
+  'Reliable',
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div>
+      <div className="flex gap-4">
+        {words.map((word) => (
+          <div key={word} className="inline-flex gap-4">
+            <span>{word}</span>
+            <StarIcon className="size-6" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
