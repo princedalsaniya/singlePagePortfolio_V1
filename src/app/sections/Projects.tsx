c1f0fa30d5d8335822a62cf1598f473a3d7bfaf3
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-26 09:41:42 +0530

    style: finished styling for mobile view

--- src/app/sections/Projects.tsx
+++ src/app/sections/Projects.tsx
@@ -1,6 +1,9 @@
 import kleoNetworkPublishCards from '@/app/assets/images/KleoNetworkPublishCardsPage.png';
 import lightSaasLandingPage from '@/app/assets/images/light-saas-landing-page.png';
 import aiStartupLandingPage from '@/app/assets/images/ai-startup-landing-page.png';
+import CheckCircleIcon from '@/app/assets/icons/check-circle.svg';
+import ArrowUpRightIcon from '@/app/assets/icons/arrow-up-right.svg';
+import grainImage from '@/app/assets/images/grain.jpg';
 import Image from 'next/image';
 
 const portfolioProjects = [
@@ -44,33 +47,57 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <div>
+    <section className="pb-16">
       <div className="container">
-        <p>Real-world Projects</p>
-        <h2>Featured Projects</h2>
-        <p>See how I transformed Figma designs into engaging digital experiences.</p>
-        <div>
+        <div className="flex justify-center">
+          {/* NOTE : Gradient Text = bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text */}
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">
+            Real-world Projects
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
+        <p className="text-center text-white/60 mt-4">
+          See how I transformed Figma designs into engaging digital experiences.
+        </p>
+        <div className="flex flex-col mt-10 gap-20">
           {portfolioProjects.map((project) => (
-            <div key={project.title}>
-              <div>
+            <div
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:content-[''] after:absolute after:inset-0 after:z-10 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none"
+              key={project.title}
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{ backgroundImage: `url(${grainImage.src})` }}
+              />
+              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text inline-flex uppercase tracking-widest font-bold text-sm gap-2">
                 <span>{project.company}</span>
+                <span>&bull;</span>
                 <span>{project.year}</span>
               </div>
-              <h3>{project.title}</h3>
-              <hr />
-              <ul>
+
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4" />
+              <ul className="flex flex-col gap-4 mt-4">
                 {project.results.map((result) => (
-                  <li>{result.title}</li>
+                  <span>
+                    <li className="flex gap-2 text-sm text-white/50">
+                      <CheckCircleIcon className="size-5 shrink-0" />
+                      {result.title}
+                    </li>
+                  </span>
                 ))}
               </ul>
               <a href={project.link}>
-                <button>View Live Site</button>
+                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold  inline-flex justify-center items-center gap-2 mt-8">
+                  <span>View Live Site</span>
+                  <ArrowUpRightIcon className="size-4" />
+                </button>
               </a>
-              <Image src={project.image} alt={project.title}></Image>
+              <Image className="mt-8 -mb-4" src={project.image} alt={project.title}></Image>
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
