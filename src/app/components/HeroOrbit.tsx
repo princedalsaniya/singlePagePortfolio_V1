07271e445b66ad408045bb3ba1ef0cc6b4f562b7
Prince Dalsaniya <43609278+princedalsaniya@users.noreply.github.com>
2024-08-27 10:11:45 +0530

    [ANIMATE] Hero section (#7)
    * style: added new animation for ping dot in AvailableForProjects
    
    * feat: hero section animation done
    
    - orbiting elements
    - spinning on their own too

--- src/app/components/HeroOrbit.tsx
+++ src/app/components/HeroOrbit.tsx
@@ -3,17 +3,38 @@ import { PropsWithChildren } from 'react';
 interface HeroOrbitProps {
   size: number;
   rotation: number;
+  className?: string;
+  orbitDuration?: number;
+  shouldOrbit?: boolean;
+  spinDuration?: number;
+  shouldSpin?: boolean;
 }
 
-export const HeroOrbit = ({ children, size, rotation }: PropsWithChildren<HeroOrbitProps>) => {
+export const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+  className,
+  orbitDuration,
+  shouldOrbit = false,
+  spinDuration,
+  shouldSpin = false,
+}: PropsWithChildren<HeroOrbitProps>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
-      <div
-        className="flex items-start justify-start"
-        style={{ height: `${size}px`, width: `${size}px`, transform: `rotate(${rotation}deg)` }}
-      >
-        <div className="inline-flex" style={{ transform: `rotate(${rotation * -1}deg)` }}>
-          {children}
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
+      <div className={shouldOrbit ? 'animate-spin' : ''} style={{ animationDuration: `${orbitDuration || 10}s` }}>
+        <div
+          className="flex items-start justify-start"
+          style={{ height: `${size}px`, width: `${size}px`, transform: `rotate(${rotation}deg)` }}
+        >
+          <div
+            className={shouldSpin ? 'animate-spin' : ''}
+            style={{ animationDuration: `${spinDuration || 10}s` }}
+          >
+            <div className="inline-flex" style={{ transform: `rotate(${rotation * -1}deg)` }}>
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
