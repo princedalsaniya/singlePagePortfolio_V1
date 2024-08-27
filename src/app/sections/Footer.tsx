34b513c4a00f1a88f5c912febb1b2824f51ebf9c
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-27 09:17:39 +0530

    style: Footer component for mobile view

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -23,15 +23,17 @@ export const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div>&copy; 2024, All rights reserved.</div>
-        <nav>
-          {footerLinks.map((link) => (
-            <a href={link.href} key={link.title}>
-              <span>{link.title}</span>
-              <ArrowUpRightIcon />
-            </a>
-          ))}
-        </nav>
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
+          <div className="text-white/40">&copy; 2024, All rights reserved.</div>
+          <nav className="flex flex-col items-center gap-8">
+            {footerLinks.map((link) => (
+              <a href={link.href} key={link.title} className="inline-flex items-center gap-1.5">
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRightIcon className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
       </div>
     </footer>
   );
