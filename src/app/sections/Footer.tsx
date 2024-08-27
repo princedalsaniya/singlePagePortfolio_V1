9f19d96a8ecbccc23e18b11208d70036f245d0f7
princedalsaniya <prince.d@ahduni.edu.in>
2024-08-27 09:13:21 +0530

    feat: added all HTML for footer

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -1,3 +1,38 @@
+import ArrowUpRightIcon from '@/app/assets/icons/arrow-up-right.svg';
+
+const footerLinks = [
+  {
+    title: 'YouTube',
+    href: '#',
+  },
+  {
+    title: 'LinkedIn',
+    href: '#',
+  },
+  {
+    title: 'Github',
+    href: '#',
+  },
+  {
+    title: 'Instagram',
+    href: '#',
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div>&copy; 2024, All rights reserved.</div>
+        <nav>
+          {footerLinks.map((link) => (
+            <a href={link.href} key={link.title}>
+              <span>{link.title}</span>
+              <ArrowUpRightIcon />
+            </a>
+          ))}
+        </nav>
+      </div>
+    </footer>
+  );
 };
